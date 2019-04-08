7773640f1020a5de4a05eb30c702e5a1f7302ad1
Alex Borst <butler.borst@gmail.com>
2019-04-08 23:47:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,9 +6,9 @@ def speak_to_grandma(phrase = "i love you grandma!")
     if phrase == phrase.upcase
       return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.downcase
-  		puts "HUH?! SPEAK UP, SONNY!"
+  		return "HUH?! SPEAK UP, SONNY!"
     else phrase == phrase.upcase
-      puts "NO, NOT SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
     end
 end
 
