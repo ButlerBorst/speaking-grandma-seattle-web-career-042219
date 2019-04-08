7849b34bfc17f433d12f01491accf402fc757bd3
Alex Borst <butler.borst@gmail.com>
2019-04-08 23:25:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,6 @@
 def speak_to_grandma(phrase = 'I LOVE YOU GRANDMA!')
   if phrase == phrase
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase
+  elsif phrase == 'Hi Nana, how are you?'
     puts "HUH?! SPEAK UP, SONNY!"
   
